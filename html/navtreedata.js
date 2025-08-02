f4298964cbea34d902029469b72a2927d673f24c
Igor Grešovnik <gresovnik@gmail.com>
2025-08-02 05:00:36 +0200

    First commit of documentation

---
+++ html/navtreedata.js
@@ -0,0 +1,126 @@
+/*
+@licstart  The following is the entire license notice for the
+JavaScript code in this file.
+
+Copyright (C) 1997-2019 by Dimitri van Heesch
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of version 2 of the GNU General Public License as published by
+the Free Software Foundation
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+@licend  The above is the entire license notice
+for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Doc: IGLib", "index.html", [
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Properties", "functions_prop.html", "functions_prop" ],
+        [ "Events", "functions_evnt.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"d0/d5a/classIG_1_1Num_1_1VectorBase.html#a9ba7ab25687f607872f6179c38de941a",
+"d0/da2/classIG_1_1Gr_1_1GraphicObject.html#ae9c83f3a6cef23cea976e2cb10534e75",
+"d1/d01/classIG_1_1Lib_1_1StringTable.html#ab26766f9dcfc0eff4429503224380ea8",
+"d1/d4d/classIG_1_1Lib_1_1IpcStreamServerBase.html#ac28804ad91575f36ce450eb89498977f",
+"d1/d77/classIG_1_1Lib_1_1ApplicationBase.html#a63126ae60a3e1ca8e8b9fc9344939acd",
+"d1/ddc/classIG_1_1Num_1_1GridGenerator1d.html#a5f1107afcd15fc2067bf7d7091559c41",
+"d2/d1b/classIG_1_1Num_1_1ConstMisc.html#a6a8578b8a331a031cfe1a66a27ef98bd",
+"d2/d80/structLearnCs_1_1Lib_1_1Complex.html#ad61d5ff71b1be78ff9d4b6b3dca13fa0",
+"d2/da7/structIG_1_1Num_1_1Experimental_1_1xdouble.html#a12a0c02a81c3dc354a5f54e2d45f37f4",
+"d3/d02/classIG_1_1Lib_1_1XmlParser.html#ad78922e712f719e32f07ff271be6d310",
+"d3/d40/classIG_1_1Lib_1_1ScriptLoaderBase.html#ad9772b292ab1056401ad756c16a497f0",
+"d3/d89/classIG_1_1Num_1_1ExampleMathClass_1_1Nested.html#a85e14610813dfee9b6e3d1f5cac1c841",
+"d3/dd9/classIGLib_1_1Core_1_1Tests_1_1CollectionTypeConverterTests.html#a756bd32e77512a277db77673cca25f4b",
+"d4/d1f/classIG_1_1Lib_1_1SerializerJson.html#ad389df96ced0879c4e94954395d122a5",
+"d4/d36/classIG_1_1Num_1_1VectorFunctionBaseGeneral_1_1ObjectStoreResults.html#a602b41b9a4b2d39e7ab4915001d02513",
+"d4/dc9/classIGLib_1_1Core_1_1CollectionExtensions_1_1CollectionExtensions.html#a1363cbcb7a34dd4bdb8663573f6447ee",
+"d4/de2/classIG_1_1Lib_1_1ReporterBase.html#ab6ba96157baae7da29e57aab606d9dcd",
+"d5/d23/classIGLib_1_1Gr3D_1_1KleinBottleFromFigureEight.html#a66b219733825dc126b71e3f64d3e6359",
+"d5/d4e/classIG_1_1Num_1_1AnalysisResults.html#a21a298ecd0e44b3014f736f92b03037b",
+"d5/d83/classIG_1_1Num_1_1RealFunction.html#ab78620c5434f849d8b94bf0c9b7e52c5",
+"d5/db7/classIG_1_1Num_1_1M.html#a79955605444eb4c9a242f9d2898e26e7",
+"d5/dd5/classIG_1_1Num_1_1InputOutputDataDefiniton.html#afb3bc1431ddd28414e530b4348a15508",
+"d5/dfd/classIGLib_1_1Parsing_1_1ParsableBase.html#a0efd0671cd5b2ff34a30a5de22153374",
+"d6/d52/classIG_1_1Num_1_1NeuralTrainingLimitsDto.html#aef2084d2a8184d673bfca13b58c77755",
+"d6/d82/classIG_1_1Num_1_1NeuralTrainingLimits.html#a153292c2776707400aea9417c90b4082",
+"d6/db1/classIGLib_1_1Core_1_1Extended_1_1TypeConversionHelper.html#ad51fcd066cb6484d8619f4d13fbeaeb9",
+"d7/d01/classIGLib_1_1Core_1_1ToStringTypeConverterViaIConvertible.html#aedf8992db011c09d868856319cd8a4d3",
+"d7/d48/classIG_1_1Num_1_1MExt.html#a76267fd7bf1e06364b38208d1841ccf4",
+"d7/d9f/classIG_1_1Lib_1_1HtmlWriter.html#a31ca939605c5c4ab1016f802a940535a",
+"d7/dca/classIG_1_1Lib_1_1Util.html#a26b1348232badba5a1ab8594f58fda3e",
+"d7/df0/classIG_1_1Lib_1_1Logger.html",
+"d8/d55/classIG_1_1Num_1_1Experimental_1_1MX.html#aaa00feafe802cf4bb9306be41629a082",
+"d8/ddb/classIG_1_1Num_1_1Func_1_1Linear.html#af479f680752fc769bfc34ba5d00d741f",
+"d9/d61/classIG_1_1Num_1_1Func_1_1WeightGauss.html#a2d9e6ca5fb41da49bfb268aa244c000b",
+"d9/da9/classIG_1_1Lib_1_1UtilSystem.html#a17fe5702ca57deb5bd718ab5e28733d2",
+"d9/db5/classIG_1_1Lib_1_1PipeServerBase.html#a78d43ea1e71fa72bfcdc2174172d9036",
+"d9/db9/classIG_1_1Num_1_1MatrixBase.html#ae95499f8c86b6ada7c251ca5c0cea977",
+"d9/dfe/classIG_1_1Num_1_1SampledDataSet_1_1ComparerOutputDistance.html",
+"da/d2d/classIG_1_1Net_1_1PortScanner2.html#a783cbfbbf3e44238805964c42d589db7",
+"da/d66/classIGLib_1_1Core_1_1Tests_1_1Celsius.html#a4b2e2e1436f968496c3da58fe781ad81",
+"da/d93/classIG_1_1Num_1_1SampledDataCsv.html#a32d519867d8b81f5baa75f5843e17102",
+"da/de7/classIGLib_1_1Core_1_1Tests_1_1ToStringTypeConverterViaJsonTests.html#a046d3381f6824a73bcdfde7956f4a7ce",
+"db/d5e/interfaceIG_1_1Num_1_1IBoundingBox.html",
+"db/da0/classIG_1_1Num_1_1NeuralTrainingParameters.html#ab6416c93ba8e4ac519b9391d984e2040",
+"db/da4/classIG_1_1Script_1_1ScriptAppBase.html#acbe660895e9d1b6fb76b4971da3936cb",
+"db/dc9/UtilCryptoAux_8cs.html#aab4f378670eb01276a3e37d1854d48b7a445d0434ad7b42aff5bab616ebb41970",
+"dc/d27/classIG_1_1Num_1_1StructuredMesh2d.html#a3a35322086fec5863295d164dfd44a84",
+"dc/d89/classIG_1_1Num_1_1NonSIUnit.html#ab20675de57013011e18c3a51b53d1f54",
+"dc/db5/classIG_1_1Num_1_1NeuralApproximatorBase.html#ae97f9201cf54e0d98254007a455515a2",
+"dc/ddf/classIG_1_1Num_1_1SpeedTestCpu.html#a73ddf2104eea8e7f524d821a849d2f55",
+"dd/d37/classIG_1_1Lib_1_1ScalarFunctionLoader.html#aab3ad74495623335ea840808ac69f8de",
+"dd/d52/structIG_1_1Lib_1_1color.html#adca0e10096d049f90313ac2cf3270f94",
+"dd/d9c/classIG_1_1Num_1_1ScalarFunctionScriptControllerBase.html#aaa6261e1ec9e5a3298f2ea8f149671aa",
+"dd/dec/classIG_1_1Lib_1_1ExpressionEvaluatorCompiled.html#a866a2b0c7347209ad815dcbe5e7916d4",
+"de/d1b/structIG_1_1Num_1_1mat2.html#ac48d41694427882b5d2eac04cbf0cd31",
+"de/dc2/classIG_1_1Lib_1_1Copy_1_1ServerStreamBase.html#a425175496b1cab83f62508d85f6b2e0d",
+"de/dcf/classIG_1_1Lib_1_1CommandLineApplicationInterpreter.html#ad8e8de772a2082a1cfaab8c4f0b7a328",
+"df/d16/structIG_1_1Num_1_1vec3.html#a2a106f5e5cd955de2e9c7982c389f441",
+"df/d61/classIG_1_1Num_1_1BoundingBoxBase.html#a6026fa95df2921876022257f95db850b",
+"df/dc7/classIG_1_1Num_1_1Func3d2dExamples_1_1Paraboloid.html#a3e0e03f2c278b3cb6866feadc49e226a",
+"dir_f827730f61e608390657344dc4d6ffe3.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
